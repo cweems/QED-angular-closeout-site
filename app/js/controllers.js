865c2f5c81ccd74ea2be8d73871226476d43c974
Charlie Weems <charlie.weems@gmail.com>
2013-09-15 19:52:17 -0400

    Move ng-click to controller
    This pulls ng-click out of the navigation links and sets up logic to
    change the filter in the controllers.js file.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,16 +7,53 @@
 function PhoneListCtrl($scope, $http) {
   $http.get('content/items.json').success(function(data) {
     $scope.items = data;
+    $scope.Filter = {type : 'Welcome'}; 
   });
 
   $http.get('content/modals.json').success(function(modalData) {
     $scope.modals = modalData;
   });
 
-  $('.portfolioFilter li a').click(function(){
-		$('.portfolioFilter .current').removeClass('current');
+  $('ul.nav li').click(function(){
+		$('ul.nav li.current').removeClass('current');
 		$(this).addClass('current');
-  }); 
+  });
+
+  $('ul.nav #first-link').click(function(){
+    $scope.Filter = {type : 'Welcome'};
+  });
+
+  $('ul.nav #second-link').click(function(){
+    $scope.Filter = {type : 'Evolution'};
+  });
+
+  $('ul.nav #third-link').click(function(){
+    $scope.Filter = {type : 'Approach'};
+  });
+
+  $('ul.nav #fourth-link').click(function(){
+    $scope.Filter = {type : 'Partners'};
+  });
+
+  $('ul.nav #fifth-link').click(function(){
+    $scope.Filter = {type : 'Communities'};
+  });
+
+  $('ul.nav #sixth-link').click(function(){
+    $scope.Filter = {type : 'Reach'};
+  });
+
+  $('ul.nav #seventh-link').click(function(){
+    $scope.Filter = {type : 'Impact'};
+  });
+
+  $('ul.nav #eighth-link').click(function(){
+    $scope.Filter = {type : 'Team'};
+  });
+
+  $('ul.nav #ninth-link').click(function(){
+    $scope.Filter = {type : 'Contact'};
+  });
 
   $(document).ready(function() {
     $(window).scroll(function() {
