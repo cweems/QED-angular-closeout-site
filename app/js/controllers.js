18a2f83c127cc8afd2bbf3c8e0e7747593b586ea
Charlie Weems <charlie.weems@gmail.com>
2013-09-17 13:55:33 -0400

    Update styple of "Next Up" button

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -26,48 +26,47 @@ function PhoneListCtrl($scope, $http) {
   })
 
 
-
   $('ul.nav li').click(function(event){
     var navId = event.target.id;
     switch(navId){
     case 'first-link':
       $scope.Filter = {type : 'Welcome'};
-      $scope.nextButton = {text: 'Next Up: Evolution of KDMD', href: '#Evolution', filter: 'Evolution'};
+      $scope.nextButton = {text: 'Next Up: Evolution of KDMD', href: '#Evolution', filter: 'Evolution', newValue: 'first-link'};
       $('body').scrollTo( $('#Welcome'), 800 );
       break;
     case 'second-link':
       $scope.Filter = {type : 'Evolution'};
-      $scope.nextButton = {text: 'Next Up: Our KM Approach', href: '#Approach', filter: 'Approach'};
+      $scope.nextButton = {text: 'Next Up: Our KM Approach', href: '#Approach', filter: 'Approach', newValue: 'first-link'};
       $('body').scrollTo( $('#Evolution'), 800 );
       break;
     case 'third-link':
       $scope.Filter = {type : 'Approach'};
-      $scope.nextButton = {text: 'Next Up: Our USAID Partners', href: '#Partners', filter: 'Partners'};
+      $scope.nextButton = {text: 'Next Up: Our US newValue Partners', href: '#Partners', filter: 'Partners', newValue: 'first-link'};
       $('body').scrollTo( $('#Approach'), 800 );
       break;
     case 'fourth-link':
       $scope.Filter = {type : 'Partners'};
-      $scope.nextButton = {text: 'Next Up: Building Communities', href: '#Communities', filter: 'Communities'};
+      $scope.nextButton = {text: 'Next Up: Building Communities', href: '#Communities', filter: 'Communities', newValue: 'first-link'};
       $('body').scrollTo( $('#Partners'), 800 );
       break;
     case 'fifth-link':
       $scope.Filter = {type : 'Communities'};
-      $scope.nextButton = {text: 'Next Up: Our Knowledge Management Reach', href: '#Reach', filter: 'Reach'};
+      $scope.nextButton = {text: 'Next Up: Our Knowledge Management Reach', href: '#Reach', filter: 'Reach', newValue: 'first-link'};
       $('body').scrollTo( $('#Communities'), 800 );
       break;
     case 'sixth-link':
       $scope.Filter = {type : 'Reach'};
-      $scope.nextButton = {text: 'Next Up: Our Impact', href: '#Impact', filter: 'Impact'};
+      $scope.nextButton = {text: 'Next Up: Our Impact', href: '#Impact', filter: 'Impact', newValue: 'first-link'};
       ('body').scrollTo( $('#Reach'), 800 );
       break;
     case 'seventh-link':
       $scope.Filter = {type : 'Impact'};
-      $scope.nextButton = {text: 'Next Up: Our Story, Our Team', href: '#Team', filter: 'Team'};
+      $scope.nextButton = {text: 'Next Up: Our Story, Our Team', href: '#Team', filter: 'Team', newValue: 'first-link'};
       ('body').scrollTo( $('#Impact'), 800 );
       break;
     case 'eighth-link':
       $scope.Filter = {type : 'Team'};
-      $scope.nextButton = {text: 'Next Up: Contact', href: '#Contact', filter: 'Contact'};
+      $scope.nextButton = {text: 'Next Up: Contact', href: '#Contact', filter: 'Contact', newValue: 'first-link'};
       ('body').scrollTo( $('#Team'), 800 );
       break;
     case 'ninth-link':
@@ -78,8 +77,6 @@ function PhoneListCtrl($scope, $http) {
       $scope.Filter = {type : 'Welcome'};
     };
   });
-
-
 
   $(document).ready(function() {
     $(window).scroll(function() {
